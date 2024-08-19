22ad0b89864adbcdb4e6f085248cd5931c4dbf0f
김정수 <kjs20151240@gmail.com>
2024-08-19 16:58:39 +0900

    test | test
    amplify test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>Hello Gong!</h1>
-        <h2>he is handsome guy</h2>
-      </div>
-    </main>
+    <>
+    <h1>Hello Gong!</h1>
+    <h2>he is handsome guy</h2>
+    <h2>Mr.Gong!</h2>
+    </>
   );
 }
