69689edef1ceb95d61322a5ed96c0a415df10b62
김정수 <kjs20151240@gmail.com>
2024-09-02 14:18:29 +0900

    modi | naviItem.tsx
    Content생성및 관리. type 지정

--- src/app/ui/navigatebar/naviLiteral/naviItem.tsx
+++ src/app/ui/navigatebar/naviLiteral/naviItem.tsx
@@ -1,3 +1,13 @@
-const naviItem : string[] = ["패치노트", "대기실", "잡담게시판"] 
+const naviItem: Array<keyof typeof naviRouteContent> = [
+  '패치노트',
+  '대기실',
+  '잡담게시판',
+];
 
-export default naviItem
+const naviRouteContent = {
+  패치노트: <p>이것은 패치 노트이다.</p>,
+  대기실: <p>대기실임</p>,
+  잡담게시판: <p>잡담임</p>,
+};
+
+export { naviItem, naviRouteContent };
