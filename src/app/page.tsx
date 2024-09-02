20968e63c67760331ec30b873fd3b921213b7f0c
김정수 <kjs20151240@gmail.com>
2024-09-02 10:59:22 +0900

    test | page.tsx
    navigatebar테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import NavigateBar from "./ui/navigatebar/navigatebar";
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
     <h1>Hello Gong!</h1>
     <h2>he is handsome guy</h2>
     <h2>Mr.Gong!</h2>
+    <NavigateBar />
     </>
   );
 }
