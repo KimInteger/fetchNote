81193ad7a956195c5197c19a9130a8b55c55cfef
김정수 <kjs20151240@gmail.com>
2024-09-02 10:59:03 +0900

    frame | navigatebar.tsx
    탭메뉴 프레임 구성

--- src/app/ui/navigatebar/navigatebar.tsx
+++ src/app/ui/navigatebar/navigatebar.tsx
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+'use client'
+
+import React, { ReactEventHandler, ReactNode, useState } from "react";
 import naviItem from "./naviLiteral/naviItem";
 
 const NavigateBar : React.FC = () => {
-  const navi = naviItem
+  const [viewContent, setViewContent] = useState<string>(naviItem[0])
+  
+  const navi : string[] = naviItem
+  
+  const changeContent = (item : string) => {
+    setViewContent(item)
+  }
   
   return(
     <>
       <ul>
         {navi.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index} onClick={()=>{changeContent(item)}}>{item}</li>
         ))}
       </ul>
+
+      {viewContent === naviItem[0] && 
+        <div>야호</div>
+      }
     </>
   )
 }
+
+export default NavigateBar
