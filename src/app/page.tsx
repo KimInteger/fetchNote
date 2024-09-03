a29c4bfbbb020d5b2e642afe09b6e96cb9455dff
김정수 <kjs20151240@gmail.com>
2024-09-04 00:21:43 +0900

    test | page.tsx
    헤더 섹션 삽입 이후 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import NavigateBar from './ui/navigatebar/navigatebar';
+import HeaderSection from './ui/header/headerSection';
 
 export default function Home() {
   return (
     <>
+      <HeaderSection />
       <NavigateBar />
     </>
   );
