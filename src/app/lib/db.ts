362cb145cb07e8b0069b5f711e0d8fc99391092f
김정수 <kjs20151240@gmail.com>
2024-09-04 18:02:35 +0900

    feat | db.ts
    SSL인증 우회

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,7 +1,11 @@
 import { Pool } from 'pg';
 
+// PostgreSQL 연결 설정
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // SSL 인증서 검증 비활성화
+  },
 });
 
 export default pool;
