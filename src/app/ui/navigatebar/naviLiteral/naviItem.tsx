c933f21ee088e4bbf7d0928ed3688a76e7613e5a
김정수 <kjs20151240@gmail.com>
2024-09-05 09:31:27 +0900

    memo | naviItem.tsx
    패치노트 => 공지사항

--- src/app/ui/navigatebar/naviLiteral/naviItem.tsx
+++ src/app/ui/navigatebar/naviLiteral/naviItem.tsx
@@ -1,11 +1,13 @@
+import RegisterForm from '../../registerForm';
+
 const naviItem: Array<keyof typeof naviRouteContent> = [
-  '패치노트',
+  '공지사항',
   '대기실',
   '잡담게시판',
 ];
 
 const naviRouteContent = {
-  패치노트: <p>이것은 패치 노트이다.</p>,
+  공지사항: <RegisterForm />,
   대기실: <p>대기실임</p>,
   잡담게시판: <p>잡담임</p>,
 };
