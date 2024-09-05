7b2fea7978f57661cd2cf9ecee4f008e149a8bec
김정수 <kjs20151240@gmail.com>
2024-09-05 10:26:28 +0900

    edit | naviItem.tsx
    공지사항에 writeFiled할당.

--- src/app/ui/navigatebar/naviLiteral/naviItem.tsx
+++ src/app/ui/navigatebar/naviLiteral/naviItem.tsx
@@ -1,4 +1,4 @@
-import RegisterForm from '../../registerForm';
+import WriteFieldSection from '../../writeField/fieldSection';
 
 const naviItem: Array<keyof typeof naviRouteContent> = [
   '공지사항',
@@ -7,7 +7,7 @@ const naviItem: Array<keyof typeof naviRouteContent> = [
 ];
 
 const naviRouteContent = {
-  공지사항: <RegisterForm />,
+  공지사항: <WriteFieldSection />,
   대기실: <p>대기실임</p>,
   잡담게시판: <p>잡담임</p>,
 };
