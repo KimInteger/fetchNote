693a28f67e2c014b8a60da6e8e5e2dbe6da82099
김정수 <kjs20151240@gmail.com>
2024-09-07 22:12:12 +0900

    create | AuthContext
    상태 관리 로그인 상태 관리를 위한
    Context

---
+++ src/app/ui/context/AuthContext.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  setIsAuthenticated: (authenticated: boolean) => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
